3deea3a2da48f5d69f5dc38e3682d2ef1e6d6d8f
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2019-02-05 11:35:22 -0200

    Add files via upload

---
+++ Pratica_Unidade_02.c
@@ -0,0 +1,29 @@
+// Código da prática da unidade 2 de Introdução a Programação (Nivelamento)
+
+#include <stdio.h>
+
+main() {
+	int codigo;
+	float precomp, imp, lucro, prevend;
+	
+	printf("\nENTRADA DE DADOS DO PRODUTO\n");
+	
+	printf("Digite o codigo do produto: ");
+	scanf("%d", &codigo);
+	printf("Digite o preco de compra: R$ ");
+	scanf("%f", &precomp);
+
+	imp = precomp * 0.35;
+	lucro = precomp * 0.47;
+	prevend = precomp + imp + lucro;
+
+	printf("\nLISTA DOS DADOS DO PRODUTO");
+
+	printf("\n+-----------------------------------------------------------------------+");
+	printf("\n| Codigo | Preco Compra | Imposto | Lucro | Preco Venda | Classificacao |");
+	printf("\n+-----------------------------------------------------------------------+");
+	printf("\n| %6d | %12.2f | %7.2f | %5.2f | %11.2f | ************* |", codigo, precomp, imp, lucro, prevend);
+	printf("\n+-----------------------------------------------------------------------+");
+
+	getch();
+}
